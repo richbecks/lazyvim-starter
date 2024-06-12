a954d37119f92fdb99bf5fa11c5286741154a088
Richard Jones <richardj@bsquare.com>
2024-06-12 09:59:05 +0100

    dadbod (db) plugin

---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,30 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
+    'pbogut/vim-dadbod-ssh',
+    lazy = true,
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+    },
+  },
+}
