01cbf4c339a1d8a6ae18087314dca4ef8d6e8f18
Richard Jones <richardj@bsquare.com>
2024-06-12 09:55:41 +0100

    Rounded border floating terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local Util = require("lazy.util")
+
+vim.keymap.set("n", "<C-_>", function()
+  Util.float_term(nil, { border = "rounded" })
+end, { desc = "Term with border" })
