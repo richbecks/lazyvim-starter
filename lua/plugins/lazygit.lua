056ed2ea0b96974ea67fe4ce32ad1fbece2403eb
Richard Jones <richardj@bsquare.com>
2024-07-25 12:23:23 +0100

    Add lazygit integration

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,12 @@
+return {
+  "kdheepak/lazygit.nvim",
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+}
